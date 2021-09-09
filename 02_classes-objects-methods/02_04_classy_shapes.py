356ee8d2a391996bf8cf5eeffab613a39a2a720c
csulva <chrissulva1@gmail.com>
2021-09-09 18:35:11 -0400

    New labs for 301 section 02

--- 02_classes-objects-methods/02_04_classy_shapes.py
+++ 02_classes-objects-methods/02_04_classy_shapes.py
@@ -5,3 +5,27 @@
 # Write methods in the appropriate class so that you can calculate
 # the area of both the rectangle and the circle, the perimeter
 # of the rectangle, and the circumference of the circle.
+
+import math
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self, length, width):
+        return length * width
+    def perimeter(self, length, width):
+        return (length * 2) + (width *2)
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self, radius):
+        return (math.pi * radius) ** 2
+    def circumference(self, radius):
+        return radius * 2 * math.pi
+
+rectangle = Rectangle(4, 5)
+circle = Circle(10)
+
+print(circle.circumference(circle.radius))
