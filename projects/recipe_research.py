0cea948a301fe88e7911ccc27e02c12ba128a3bc
csulva <chrissulva1@gmail.com>
2021-09-10 19:20:23 -0400

    Completed the recipe/soup project in section 2 of 301

--- projects/recipe_research.py
+++ projects/recipe_research.py
@@ -1,3 +1,28 @@
 # Use your `Ingredients` class to create a URL to an online search
 # that allows to look for recipes for dishes made from the
 # available ingredients.
+import webbrowser
+
+#Create Ingredients Class with name and amount
+class Ingredient:
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+    
+    def __str__(self):
+        return f'{self.name}: {self.amount}'
+
+#Create instance method .get_info() that takes .name attribute of an Ingredient() and creates a Wikipedia URL
+    def get_info(self):
+        return webbrowser.open(f'https://en.wikipedia.org/wiki/{self.name}')
+
+    def __add__(self, other):
+        soup = self.name + ' & ' + other.name
+        min_amount = min(self.amount, other.amount)
+        return f'You can make {min_amount} servings of {soup} soup!'
+
+
+onion = Ingredient('onion', 4)
+carrot = Ingredient('carrot', 5)
+
+print(onion + carrot)
