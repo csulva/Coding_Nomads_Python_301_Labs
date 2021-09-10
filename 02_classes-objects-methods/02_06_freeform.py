04778b4ea4c9cf5d1fefe6c0c0648b0cdabb3db9
csulva <chrissulva1@gmail.com>
2021-09-10 18:50:19 -0400

    Completing 02_06 exercise for 301 class

--- 02_classes-objects-methods/02_06_freeform.py
+++ 02_classes-objects-methods/02_06_freeform.py
@@ -11,3 +11,56 @@
 # Using objects you can model anything you want:
 # Animals, paintings, card games, sports teams, trees, people etc...
 
+class Animal:
+    def __init__(self, name, integument, speech, consumption, blood):
+        self.name = name
+        self.integument = integument
+        self.speech = speech
+        self.consumption = consumption
+        self.blood = blood
+        
+    def __str__(self):
+        return f'The {self.name} has {self.integument}, goes {self.speech}, eats {self.consumption}, and is {self.blood} blooded.'
+
+    def __add__(self, other):
+        return f'The {self.name} and {other.name} together have {self.legs + other.legs} legs.'
+
+class Dog(Animal):
+    def __init__(self, name, integument, speech, consumption, blood, breed, legs):
+        super().__init__(name, integument, speech, consumption, blood)
+        self.breed = breed
+        self.legs = legs
+    
+    def __str__(self):
+        return f'The {self.breed} breed of {self.name} has {self.integument}, goes {self.speech}, eats {self.consumption}, is {self.blood} blooded, and has {self.legs} legs.'
+
+    def __add__(self, other):
+        return ('total legs: ' + f'{(self.legs + other.legs)}')
+    
+class Bird(Animal):
+    def __init__(self, name, integument, speech, consumption, blood, type, native):
+        super().__init__(name, integument, speech, consumption, blood)
+        self.type = type
+        self.native = native
+    def __str__(self):
+        return f'The {self.type} type of {self.name} has {self.integument}, goes {self.speech}, eats {self.consumption}, is {self.blood} blooded, and native to {self.native}.'
+
+class Snake(Animal):
+    def __init__(self, name, integument, speech, consumption, blood, type, legs):
+        super().__init__(name, integument, speech, consumption, blood)
+        self.type = type
+        self.legs = legs
+    def __str__(self):
+        return f'The {self.type} type of {self.name} has {self.integument}, goes {self.speech}, eats {self.consumption}, is {self.blood} blooded, and has {self.legs} legs.'
+    def __add__(self, other):
+        return super().__add__(other)
+
+dog = Animal('dog', 'fur', 'bark', 'meat, plants', 'warm')
+golden = Dog('dog', 'fur', 'ruff', 'meat, plants', 'warm', 'Golden Retriever', 4)
+beagle = Dog('dog', 'fur', 'bark', 'plants', 'warm', 'Beagle', 4)
+toucan = Bird('bird', 'feathers', 'croak', 'bugs, plants', 'warm', 'toucan', 'South America')
+kiwi = Bird('bird', 'feathers', 'chirp', 'bugs, plants', 'warm', 'kiwi', 'Australia')
+viper = Snake('snake', 'scales', 'hiss', 'birds, rodents, reptiles', 'cold', 'viper', 0)
+python = Snake('snake', 'scales', 'hiss', 'birds, rodents, reptiles', 'cold', 'python', 0)
+
+print(golden + python)
