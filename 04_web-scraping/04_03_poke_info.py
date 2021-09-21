edc61dd458730a3fb1fe2493ad7dcd1f845ddd4c
csulva <chrissulva1@gmail.com>
2021-09-21 19:26:52 -0400

    New labs for web scraping

--- 04_web-scraping/04_03_poke_info.py
+++ 04_web-scraping/04_03_poke_info.py
@@ -10,5 +10,27 @@
 # You can improve on your project even more by writing the data to a small
 # `.html` page which allows you to also display the sprites of each Pokémon.
 # Check out the guides they provide: https://pokeapi-how.appspot.com/page5
+import requests
+import json
 
-BASE_URL = "https://pokeapi.co/api/v2/"
+BASE_URL = "https://pokeapi.co/api/v2/pokemon"
+request = requests.get(BASE_URL)
+data = request.json()
+
+for k, v in data.items():
+    if k == 'results':
+        for key, val in v[0].items():
+            if key == 'url':
+                new_url = val
+
+new_url = new_url[0:-2]
+
+for num in range(0, 152):
+    new = new_url + f'{num}'
+    response = requests.get(new)
+    # new_data = response.json()
+    # for k, v in new_data.items():
+    #     if k == 'forms':
+    #         for kay, vee in v[0]:
+    #             if kay['name'] == 'bulbasaur':
+    #                 print(kay, vee)
--- 04_web-scraping/04_05_wiki_scrape.py
+++ 04_web-scraping/04_05_wiki_scrape.py
@@ -4,5 +4,29 @@
 # Programmatically follow one of the links that lead to another Wikipedia article,
 # extract the text content from that article, and save it to a local text file.
 # BONUS TASK: Use RegExp to find all numbers in the text.
+import requests
+from bs4 import BeautifulSoup
+import json
+import pprint
 
 URL = "https://en.wikipedia.org/wiki/Web_scraping"
+response = requests.get(URL)
+data = BeautifulSoup(response.text)
+
+content = data.find('div', id='bodyContent')
+links = content.find_all('a', class_=None)
+list_of_links = []
+for link in links:
+    if '#' not in link['href']:
+        list_of_links.append(link)
+
+new_link = list_of_links[21].text
+new_url = new_link.replace(' ', '_')
+new_url = f'https://en.wikipedia.org/wiki/{new_url}'
+
+new_response = requests.get(new_url)
+new_data = BeautifulSoup(new_response.text)
+text = new_data.find('div', class_='vector-body')
+
+# with open('wiki_content.txt', 'w') as fout:
+#     json.dump(text.text, fout)
