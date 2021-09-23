e4c7b4a98a07d95ec20ddf111c459f64b744b29f
csulva <chrissulva1@gmail.com>
2021-09-23 19:52:17 -0400

    Initial commit for 06 module labs on Testing

--- 06_testing/06_01_test_math_module.py
+++ 06_testing/06_01_test_math_module.py
@@ -1,2 +1,14 @@
 # Write a unittest test suite with at least two methods that test
 # the functionality of the built-in `math` module.
+
+import unittest
+import math
+
+class TestMath(unittest.TestCase):
+    def testSqrt(self):
+        self.assertEqual(math.sqrt(36), 6)
+    def testFloor(self):
+        self.assertEqual(math.floor(5.4), 5)
+
+if __name__ == "__main__":
+    unittest.main()
