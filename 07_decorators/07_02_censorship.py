4496d0abb9c951542c1b7592799e08711ec1616b
csulva <chrissulva1@gmail.com>
2021-09-28 19:35:30 -0400

    Committing new labs from python 301 course on decorators - section 07

--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -1,6 +1,18 @@
 # Create a decorator that censors potentially offensive words from a text.
 # For example, assuming that "shoot" was considered an offensive word:
-# A function that would normall return this text:
+# A function that would normally return this text:
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+def censor(function):
+    def wrapper_func():
+        new_func = str(function()).replace('hoot', '****')
+        print(new_func)
+    return wrapper_func
+
+@censor
+def curse():
+    return "I stubbed my toe, shoot!"
+
+curse()
--- 07_decorators/07_03_text_decoration.py
+++ 07_decorators/07_03_text_decoration.py
@@ -7,3 +7,18 @@
 # ******************************
 # Hello
 # ******************************
+
+def symbol(symbol):
+    def decorate(function):
+        def wrapper(msg):
+            print(symbol+symbol+symbol+symbol+symbol+symbol+symbol+symbol)
+            print(function(msg))
+            print(symbol+symbol+symbol+symbol+symbol+symbol+symbol+symbol)
+        return wrapper
+    return decorate
+
+@symbol('<(^^,)>')
+def write_something(msg):
+    return msg
+
+write_something('Kirby is back')
